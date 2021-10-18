e5b678c7226dfea851bb9dbb9c3b5d3d5cbecede
Sakil Khan <sakilk130@gmail.com>
2021-10-18 15:39:52 +0600

    complete tinder card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header';
 import './styles/App.css';
+import TinderCards from './components/TinderCards';
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
       <Header />
+      <Router>
+        {/* Header */}
+        <Switch>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+          <Route path="/chat">Chat Page</Route>
+        </Switch>
 
-      {/* Tinder Cards */}
-      {/* Buttons below tinder cards */}
+        {/* Tinder Cards */}
+        {/* Buttons below tinder cards */}
 
-      {/* Carts screen */}
-      {/* individual chat screen */}
+        {/* Carts screen */}
+        {/* individual chat screen */}
+      </Router>
     </div>
   );
 }
