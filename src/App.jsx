17f3345bbad444550dc04cfbf7fa67f4df315b7c
Sakil Khan <sakilk130@gmail.com>
2021-10-18 12:55:50 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      {/* Tinder Cards */}
+      {/* Buttons below tinder cards */}
+
+      {/* Carts screen */}
+      {/* individual chat screen */}
+      <Header />
+    </div>
+  );
+}
+
+export default App;
